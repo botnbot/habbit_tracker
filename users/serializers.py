7fdc53db3f62deb82f5981c0ed1b4d79192ad320
Sergey Korobeynikov <skoro@smn35.com>
2026-05-05 19:15:05 +0300

    init server started PostgreSQL connected models create migrate serializers, views & URL setup

---
+++ users/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели пользователя.
+    """
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'telegram_chat_id']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        if 'telegram_chat_id' in validated_data:
+            user.telegram_chat_id = validated_data['telegram_chat_id']
+            user.save()
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Кастомный сериализатор для JWT с дополнительными полями.
+    """
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        token['email'] = user.email
+        return token
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    """
+    Регистрация нового пользователя.
+    POST /api/register/
+    """
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    """
+    Получение JWT токена.
+    POST /api/token/
+    """
+    serializer_class = CustomTokenObtainPairSerializer
